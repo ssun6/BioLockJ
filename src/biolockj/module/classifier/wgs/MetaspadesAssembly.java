f555d113b8ec7fead176d7a6d90ca29a9520d762
ssun6 <ssun5@uncc.edu>
2019-08-13 17:13:21 -0400

    add metabat and checkm

--- src/biolockj/module/classifier/wgs/MetaspadesAssembly.java
+++ src/biolockj/module/classifier/wgs/MetaspadesAssembly.java
@@ -1,8 +1,8 @@
 /**
  * @UNCC Fodor Lab
- * @author Michael Sioda
- * @email msioda@uncc.edu
- * @date Feb 9, 2017
+ * @author Shan Sun
+ * @email ssun5@uncc.edu
+ * @date Aug 12, 2019
  * @disclaimer This code is free software; you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any
  * later version, provided that any use properly credits the author. This program is distributed in the hope that it
@@ -36,16 +36,7 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 	 */
 	@Override
 	public List<List<String>> buildScript( final List<File> files ) throws Exception {
-		final List<List<String>> data = new ArrayList<>();
-		for( final File file: files ) {
-			final String fileId = SeqUtil.getSampleId( file.getName() );
-			final String outputFile = getOutputDir().getAbsolutePath() + File.separator + fileId +"_assembly";
-			final ArrayList<String> lines = new ArrayList<>();
-			lines.add(FUNCTION_METASPADES + " " + file.getAbsolutePath() + " " + outputFile );
-			data.add( lines );
-		}
-
-		return data;
+		return null;
 	}
 
 	/**
@@ -83,9 +74,14 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 	 * Metaspades runs python scripts, so no special command is required
 	 * @throws ConfigViolationException 
 	 */
-	@Override
-	public String getClassifierExe() throws ConfigViolationException {
-		return Config.getExe( this, EXE_METASPADES );
+	public String getClassifier1Exe() throws ConfigViolationException {
+		return Config.getExe(this, EXE_METASPADES );
+	}
+	public String getClassifier2Exe() throws ConfigViolationException {
+		return Config.getExe(this, EXE_METABAT2 );
+	}
+	public String getClassifier3Exe() throws ConfigViolationException {
+		return Config.getExe(this, EXE_CHECKM );
 	}
 
 	/**
@@ -105,9 +101,11 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 		final List<String> lines = super.getWorkerScriptFunctions();
 		lines.add( "function " + FUNCTION_METASPADES + "() {" );
 		if (Config.getBoolean( this, Constants.INTERNAL_PAIRED_READS )) {
-			lines.add(getClassifierExe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " -m " + getMemory() + " -1 $1 -2 $2 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $3" );
+			lines.add("#"+getClassifier1Exe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " -m " + getMemory() + " -1 $1 -2 $2 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $3/assembly" );
+			lines.add("#"+getClassifier2Exe() + " -v -m 2000  -i $3/assembly/contigs.fasta -o $3/bins/bin");
+			lines.add("#"+getClassifier3Exe() + " lineage_wf -f $3/CheckM.txt -x fa -t "+ Config.getNonNegativeInteger(this, "script.numThreads")+ " s3/bins/ $3/SCG ");
 		}else {
-			lines.add(getClassifierExe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " -m " + getMemory() + " -s $1 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $2" );
+			lines.add("#"+getClassifierExe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " s3/bins/ $3/SCG " + getMemory() + " -s $1 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $2" );
 		}		
 		lines.add( "}" + RETURN );
 		return lines;
@@ -116,7 +114,6 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 	public int getMemory(){
 		return 150;
 	}
-
 	/**
 	 * MetaSPAdes doesn't require parameters besides input files and output directory.
 	 * Default threads is 16 and memory is 250G<br>
@@ -124,52 +121,13 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 	 * @return runtime parameters
 	 * @throws Exception if errors occur
 	 */
-//	protected String getParams() throws Exception {
-////		if( this.defaultSwitches == null ) {
-////			final String params = BioLockJUtil.join( getClassifierParams() );
-////
-//////			if( params.indexOf( "--input_type " ) > -1 )
-//////				throw new Exception( "Invalid classifier option (--input_type) found in property("
-//////					+ EXE_METASPADES_PARAMS + "). BioLockJ derives this value by examinging one of the input files." );
-//////			if( params.indexOf( NUM_THREADS_PARAM ) > -1 )
-//////				throw new Exception( "Ignoring nvalid classifier option (" + NUM_THREADS_PARAM + ") found in property("
-//////					+ EXE_METASPADES_PARAMS + "). BioLockJ derives this value from property: " + SCRIPT_NUM_THREADS );
-//////			if( params.indexOf( "--bowtie2out " ) > -1 )
-//////				throw new Exception( "Invalid classifier option (--bowtie2out) found in property("
-//////					+ EXE_METASPADES_PARAMS + "). BioLockJ outputs bowtie2out files to Metaphlan2Classifier/temp." );
-//////			if( params.indexOf( "-t rel_ab_w_read_stats " ) > -1 )
-//////				throw new Exception( "Invalid classifier option (-t rel_ab_w_read_stats). BioLockJ hard codes this "
-//////					+ "option for MetaPhlAn so must not be included in the property file." );
-//////			if( params.indexOf( "--tax_lev " ) > -1 )
-//////				throw new Exception( "Invalid classifier option (--tax_lev) found in property(" + EXE_METASPADES_PARAMS
-//////					+ "). BioLockJ sets this value based on: " + Constants.REPORT_TAXONOMY_LEVELS );
-//////			if( params.indexOf( "-s " ) > -1 ) throw new Exception( "Invalid classifier option (-s) found in property("
-//////				+ EXE_METASPADES_PARAMS + "). SAM output not supported.  BioLockJ outputs " + TSV_EXT + " files." );
-//////			if( params.indexOf( "-o " ) > -1 )
-//////				throw new Exception( "Invalid classifier option (-o) found in property(" + EXE_METASPADES_PARAMS
-//////					+ "). BioLockJ outputs results to: " + getOutputDir().getAbsolutePath() + File.separator );
-//////
-//////			this.defaultSwitches = getRuntimeParams( getClassifierParams(), NUM_THREADS_PARAM )
-//////				+ "-t rel_ab_w_read_stats ";
-////
-////			if( TaxaUtil.getTaxaLevels().size() == 1 )
-////				this.defaultSwitches += "--tax_lev " + this.taxaLevelMap.get( TaxaUtil.getTaxaLevels().get( 0 ) ) + " ";
-////		}
-////
-////		return this.defaultSwitches;
-//	}
-	
-//
-//	private String getWorkerFunctionParams() throws Exception {
-////		return " " + getParams() + INPUT_TYPE_PARAM + SeqUtil.getSeqType() + " ";
-//	}
-//
-	private String defaultSwitches = null;
 
 	/**
 	 * {@link biolockj.Config} exe property used to obtain the metaphlan2 executable
 	 */
 	protected static final String EXE_METASPADES = "exe.metaspades";
+	protected static final String EXE_METABAT2 = "exe.metabat2";
+	protected static final String EXE_CHECKM = "exe.checkm";
 
 	/**
 	 * Name of the metaspades function used to assemble reads: {@value #FUNCTION_METASPADES}
@@ -186,6 +144,12 @@ public class MetaspadesAssembly extends ClassifierModuleImpl {
 	public File getDB() throws ConfigPathException, ConfigNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public String getClassifierExe() throws ConfigViolationException {
+		// TODO Auto-generated method stub
+		return Config.getExe(this, EXE_METASPADES);
 	}
 
 	/**
