c8ef3fa2b3d72459469cdf6a89a3cfb7a48f4873
ssun6 <ssun5@uncc.edu>
2019-08-22 13:12:15 -0400

    removed from classifier package and created a new package

--- src/biolockj/module/assembly/GenomeAssembly.java
+++ src/biolockj/module/assembly/GenomeAssembly.java
@@ -9,16 +9,14 @@
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details at http://www.gnu.org *
  */
-package biolockj.module.classifier.wgs;
+package biolockj.module.assembly;
 
 import java.io.File;
 import java.util.*;
 import biolockj.Config;
 import biolockj.Constants;
-import biolockj.exception.ConfigNotFoundException;
-import biolockj.exception.ConfigPathException;
 import biolockj.exception.ConfigViolationException;
-import biolockj.module.classifier.ClassifierModuleImpl;
+import biolockj.module.SeqModuleImpl;
 import biolockj.util.*;
 
 /**
@@ -26,7 +24,7 @@ import biolockj.util.*;
  * 
  * @blj.web_desc MetaSPAdes Classifier
  */
-public class GenomeAssembly extends ClassifierModuleImpl {
+public class GenomeAssembly extends SeqModuleImpl {
 	/**
 	 * Build bash script lines to assemble paired WGS reads with MetaSPAdes. The inner list contains 1 bash script line
 	 * per sample.
@@ -54,7 +52,7 @@ public class GenomeAssembly extends ClassifierModuleImpl {
 			final String fileId = SeqUtil.getSampleId( file.getName() );
 			final String outputFile = getOutputDir().getAbsolutePath() + File.separator + fileId +"_assembly";
 			final ArrayList<String> lines = new ArrayList<>();
-			lines.add( FUNCTION_METASPADES + " " + file.getAbsolutePath() + " " + map.get( file ).getAbsolutePath()
+			lines.add( FUNCTION_ASSEMBLY + " " + file.getAbsolutePath() + " " + map.get( file ).getAbsolutePath()
 				+ " " + outputFile );
 			data.add( lines );
 		}
@@ -68,26 +66,29 @@ public class GenomeAssembly extends ClassifierModuleImpl {
 	 */
 	@Override
 	public void checkDependencies() throws Exception {
+		if (!Config.getBoolean( this, Constants.INTERNAL_PAIRED_READS )) {
+			System.err.println("Metaspades doesn't support single reads.");
+		}
+		
 	}
 
 	/**
 	 * Metaspades runs python scripts, so no special command is required
 	 * @throws ConfigViolationException 
 	 */
-	public String getClassifier1Exe() throws ConfigViolationException {
+	public String getMetaspadesExe() throws ConfigViolationException {
 		return Config.getExe(this, EXE_METASPADES );
 	}
-	public String getClassifier2Exe() throws ConfigViolationException {
+	public String getMetabatExe() throws ConfigViolationException {
 		return Config.getExe(this, EXE_METABAT2 );
 	}
-	public String getClassifier3Exe() throws ConfigViolationException {
+	public String getCheckmExe() throws ConfigViolationException {
 		return Config.getExe(this, EXE_CHECKM );
 	}
 
 	/**
 	 * Obtain the metaspades runtime params
 	 */
-	@Override
 	public List<String> getClassifierParams() {
 		final List<String> params = Config.getList( this, EXE_METASPADES_PARAMS );
 		return params;
@@ -99,15 +100,14 @@ public class GenomeAssembly extends ClassifierModuleImpl {
 	@Override
 	public List<String> getWorkerScriptFunctions() throws Exception {
 		final List<String> lines = super.getWorkerScriptFunctions();
-		lines.add( "function " + FUNCTION_METASPADES + "() {" );
+		lines.add( "function " + FUNCTION_ASSEMBLY + "() {" );
 		if (Config.getBoolean( this, Constants.INTERNAL_PAIRED_READS )) {
-			lines.add(getClassifier1Exe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " -m " + getMemory() + " -1 $1 -2 $2 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $3/assembly" );
-			lines.add(getClassifier2Exe() + " -v -m 2000  -i $3/assembly/contigs.fasta -o $3/bins/bin");
-			lines.add(getClassifier3Exe() + " lineage_wf -f $3/CheckM.txt -x fa -t "+ Config.getNonNegativeInteger(this, "script.numThreads")+ " $3/bins/ $3/SCG ");
+			lines.add(getMetaspadesExe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " -m " + getMemory() + " -1 $1 -2 $2 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $3/assembly" );
+			lines.add(getMetabatExe() + " -v -m 2000  -i $3/assembly/contigs.fasta -o $3/bins/bin");
+			lines.add(getCheckmExe() + " lineage_wf -f $3/CheckM.txt -x fa -t "+ Config.getNonNegativeInteger(this, "script.numThreads")+ " $3/bins/ $3/SCG ");
 		}else {
-			lines.add(getClassifierExe() + " -t " + Config.getNonNegativeInteger(this, "script.numThreads") + " s3/bins/ $3/SCG " + getMemory() + " -s $1 --tmp-dir " + getTempDir().getAbsolutePath() + " -o $2" );
-		}		
-		lines.add( "}" + RETURN );
+			System.err.println("Metaspades doesn't support single reads.");
+	    }
 		return lines;
 	}
 	//TODO get memory from config file
@@ -130,30 +130,14 @@ public class GenomeAssembly extends ClassifierModuleImpl {
 	protected static final String EXE_CHECKM = "exe.checkm";
 
 	/**
-	 * Name of the metaspades function used to assemble reads: {@value #FUNCTION_METASPADES}
+	 * Name of the metaspades function used to assemble reads: {@value #FUNCTION_ASSEMBLY}
 	 */
-	protected static final String FUNCTION_METASPADES = "runMetaspades";
+	protected static final String FUNCTION_ASSEMBLY = "runAssembly";
 
 
 	/**
-	 * {@link biolockj.Config} List property used to obtain the metaphlan2 executable params
+	 * {@link biolockj.Config} List property used to obtain the assembly executable params
 	 */
 	protected static final String EXE_METASPADES_PARAMS = "exe.metaspadesParams";
 
-	@Override
-	public File getDB() throws ConfigPathException, ConfigNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getClassifierExe() throws ConfigViolationException {
-		// TODO Auto-generated method stub
-		return Config.getExe(this, EXE_METASPADES);
-	}
-
-	/**
-	 * {@link biolockj.Config} Directory property containing alternate database: {@value #METASPADES_DB}<br>
-	 * Must always be paired with {@value #METASPADES_MPA_PKL}
-	 */
 }
